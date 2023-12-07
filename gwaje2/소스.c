324483529bfff5f5ed8546dc32160f2b8cb4c3d0
jimin <baekjimin0728@gmail.com>
2023-12-08 06:31:07 +0900

    21

--- gwaje2/소스.c
+++ gwaje2/소스.c
@@ -8,11 +8,12 @@ void main() {
 	int try;
 	srand(time(NULL));
 	answer = rand() % 100 + 1;
-
+	printf("1부터 100까지의 수 중에서 입력해주세요.\n");
 	while (Number = answer) {
 		scanf_s("%d", &Number);
 		if (Number > answer) printf("DOWN\n");
 		else printf("UP\n");
 	}
+	printf("정답입니다!");
 	return 0;
 }
