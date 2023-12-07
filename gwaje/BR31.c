46c122d1412180367567f75153aa3047b87a9d69
jimin <baekjimin0728@gmail.com>
2023-12-07 10:12:21 +0900

    3

--- gwaje/BR31.c
+++ gwaje/BR31.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
 
 void main() {
 	int cNumber = 0;
@@ -9,7 +10,9 @@ void main() {
 	printf("베스킨라빈스31 게임을 시작합니다.");
 	random = rand();
 	if (random % 2 == 1) {
-
+		printf("당신이 선입니다.\n1부터 3까지의 수 중에서 몇 개를 입력하시겠어요?");
+		scanf("%d", uNumber);
+		printf("You: ");
 	}
 	else {
 
