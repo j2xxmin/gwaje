92ad929c76b1fc24190c52fb1a4b24170df4f7f4
jimin <baekjimin0728@gmail.com>
2023-12-07 10:23:12 +0900

    4

--- gwaje/BR31.c
+++ gwaje/BR31.c
@@ -7,14 +7,18 @@ void main() {
 	int uNumber = 0;
 	int random = 0;
 
-	printf("베스킨라빈스31 게임을 시작합니다.");
+	printf("베스킨라빈스31 게임을 시작합니다.\n\n");
 	random = rand();
 	if (random % 2 == 1) {
-		printf("당신이 선입니다.\n1부터 3까지의 수 중에서 몇 개를 입력하시겠어요?");
-		scanf("%d", uNumber);
+		printf("당신이 선입니다.\n1부터 3까지의 수 중에서 몇 개를 입력하시겠어요?\n");
+		scanf_s("%d", uNumber);
 		printf("You: ");
+		for (int i = 0; i > uNumber; i++) {
+			++cNumber;
+			printf("%d\t", cNumber);
+		}
 	}
 	else {
-
+		printf("컴퓨터가 선입니다.");
 	}
 }
