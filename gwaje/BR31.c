5cfeaaab62ae75e7aa9377f89db25440e9ddc18e
jimin <baekjimin0728@gmail.com>
2023-12-07 10:01:56 +0900

    2

--- gwaje/BR31.c
+++ gwaje/BR31.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void main() {
+	int cNumber = 0;
+	int uNumber = 0;
+	int random = 0;
 
+	printf("베스킨라빈스31 게임을 시작합니다.");
+	random = rand();
+	if (random % 2 == 1) {
+
+	}
+	else {
+
+	}
 }
